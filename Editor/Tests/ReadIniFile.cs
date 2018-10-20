fb7eb21a2563d0b57ca7b6dece336399eb81aa11
Morgan Willcock <morganwillcock@users.noreply.github.com>
2018-10-20 14:03:10 +0100

    More tests

--- Editor/Tests/ReadIniFile.cs
+++ Editor/Tests/ReadIniFile.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Xunit;
+﻿using Xunit;
 using AGS.Editor.Utils;
 
 namespace Tests
@@ -24,6 +22,52 @@ namespace Tests
             IniFile ini = new IniFile(filename);
             string expected = "";
             string actual = ini.GetValue("misc", "user_data_dir");
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DumpFile()
+        {
+            IniFile ini = new IniFile(filename);
+            string expected = @"[sound]
+digiid=-1
+midiid=-1
+digiwin=-1
+midiwin=-1
+digiindx=0
+midiindx=0
+digiwinindx=0
+midiwinindx=0
+
+[misc]
+game_width=1600
+game_height=1200
+gamecolordepth=32
+antialias=0
+notruecolor=0
+cachemax=131072
+user_data_dir=
+shared_data_dir=
+titletext=My Game Title
+
+[graphics]
+driver=Software
+windowed=0
+screen_def=max
+game_scale_fs=max_round
+game_scale_win=max_round
+filter=stdscale
+vsync=1
+render_at_screenres=0
+
+[language]
+translation=
+
+[mouse]
+auto_lock=0
+speed=1
+";
+            string actual = ini.ToString();
             Assert.Equal(expected, actual);
         }
     }
---
+++ Editor/Tests/WriteIniFile.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Xunit;
+using AGS.Editor.Utils;
+
+namespace Tests
+{
+    public class WriteIniFile
+    {
+        static string filename = "./Resources/acsetup.cfg";
+        static string temppath = Path.GetTempPath();
+
+        [Fact]
+        public void WriteFile()
+        {
+            string writepath = Path.Combine(temppath, Path.GetRandomFileName());
+            File.Copy(filename, writepath, false);
+            IniFile ini;
+
+            ini = new IniFile(writepath);
+            string expected = ini.ToString();
+
+            // just write it with no changes
+            ini.Commit();
+
+            ini = new IniFile(writepath);
+            string actual = ini.ToString();
+
+            Assert.Equal(expected, actual);
+            File.Delete(writepath);
+        }
+    }
+}
+
